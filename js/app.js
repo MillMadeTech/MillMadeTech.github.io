b98fe60a8a66dae1cd46bcec6ce0580090e81736
MillMadeTech <millmadetech@gmail.com>
2024-05-22 16:14:21 -0400

    Layout Changes

--- js/app.js
+++ js/app.js
@@ -5,3 +5,25 @@ menu.addEventListener('click', function (){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 })
+
+
+
+/*
+$(document).ready(function() {
+  $("#navbar-placeholder").load("./pages/navbar.html", function(response, status, xhr) {
+    if (status === "error") {
+      console.error("Error loading nav.html: ", xhr.status, xhr.statusText);
+    } else {
+      console.log("nav.html loaded successfully");
+
+      const menu = document.querySelector('#mobile-menu');
+      const menuLinks = document.querySelector('.navbar-menu');
+
+      menu.addEventListener('click', function() {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+      });
+    }
+  });
+});
+*/
